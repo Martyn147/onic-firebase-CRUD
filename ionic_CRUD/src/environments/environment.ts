66cc0d4ad27684b6a915d996774aeb5fbec96290
Martyn147 <85313351+Martyn147@users.noreply.github.com>
2023-06-07 15:23:38 -0500

    CRUD DE CONTACTOS
    funciones basicas sin estilos personalizados

---
+++ ionic_CRUD/src/environments/environment.ts
@@ -0,0 +1,26 @@
+// This file can be replaced during build by using the `fileReplacements` array.
+// `ng build` replaces `environment.ts` with `environment.prod.ts`.
+// The list of file replacements can be found in `angular.json`.
+
+export const environment = {
+  production: false,
+  firebaseConfig: {
+    apiKey: "AIzaSyAuw-1nqwUU0tWmIZBtmP2CDdOC3N1Bbkw",
+    authDomain: "app2023a-6a6d4.firebaseapp.com",
+    databaseURL: "https://app2023a-6a6d4-default-rtdb.firebaseio.com",
+    projectId: "app2023a-6a6d4",
+    storageBucket: "app2023a-6a6d4.appspot.com",
+    messagingSenderId: "715579431222",
+    appId: "1:715579431222:web:5a36aa690fd36ab5a6bb97",
+    measurementId: "G-TFRMGZNV3Y"
+  }
+};
+
+/*
+ * For easier debugging in development mode, you can import the following file
+ * to ignore zone related error stack frames such as `zone.run`, `zoneDelegate.invokeTask`.
+ *
+ * This import should be commented out in production mode because it will have a negative impact
+ * on performance if an error is thrown.
+ */
+// import 'zone.js/plugins/zone-error';  // Included with Angular CLI.
